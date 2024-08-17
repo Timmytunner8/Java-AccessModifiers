ed038e6cf13a218864f172c31ff46f397612b9fa
Timmytunner <timmytunnereight@gmail.com>
2024-08-17 15:15:50 -0600

    Cap 1(Access Modifiers)

---
+++ AccessModifiers/src/cat/species/Lynx.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cat.species;
+
+import cat.BigCat;
+
+/**
+ *
+ * @author timmy
+ */
+public class Lynx extends BigCat{
+    public static void main(String[] args) {
+        BigCat c = new BigCat();
+        c.
+    }
+    
+}
